54de9ac75fb9e897bb0175dd949f833e7c2ea725
Andrey Basharin <a.basharin@runetsoft.ru>
2021-02-09 20:43:06 +0300

    added routing

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="app-main-layout">
+
+    <Navbar @click-huick="isOpen = !isOpen"/>
+
+    <Sidebar v-model="isOpen"/>
+
+    <main class="app-content" :class="{full: !isOpen}">
+      <router-view/>
+    </main>
+
+    <div class="fixed-action-btn">
+      <router-link to="/record" class="btn-floating btn-large blue" href="#">
+        <i class="large material-icons">add</i>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Sidebar from '../components/app/Sidebar'
+  import Navbar from '../components/app/Navbar'
+
+  export default {
+    name: "main-layout",
+    data: () => ({
+      isOpen: true
+    }),
+    components: {
+      Sidebar, Navbar
+    }
+  }
+
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
+import router from './router'
+import {dateFilter} from './filters/date.filter.js'
+import 'materialize-css/dist/js/materialize.min.js'
 
 Vue.config.productionTip = false
+Vue.filter('date', dateFilter)
 
 new Vue({
-  render: h => h(App),
+  store,
+  router,
+  render: h => h(App)
 }).$mount('#app')
